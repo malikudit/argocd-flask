b24cf4c2d8e60842abb91932d9c84e2697d5617b
Udit Malik <udit.malik@vanderbilt.edu>
2021-06-11 23:50:17 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"Message: ": "Hello World!"})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
